312032e42e4d1c77062a9e7439d2f8beb4bd3b1a
serkan <serkan@serkan>
2024-03-06 20:16:20 +0300

    Business Katmanına Yeni Metotlar Eklendi.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,12 +4,22 @@ using DataAccess.Concrete.InMemory;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //open closed principle
+
     internal class Program
     {
         static void Main(string[] args)
         {
             ProductManager productManager = new ProductManager(new EFProductDal());
-            foreach(var product in productManager.GetAll())
+            foreach(var product in productManager.GetAllByCategoryId(2))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------");
+
+            foreach (var product in productManager.GetByUnitPrice(50,100))
             {
                 Console.WriteLine(product.ProductName);
             }
