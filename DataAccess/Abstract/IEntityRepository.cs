25e35d603194751b519d0ed137fd90f8bf3f08e0
serkan <serkan@serkan>
2024-03-06 13:58:01 +0300

    Entity Framework  eklendi.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //Generic constraint
+    //class: referans Tip olabilir yani entitilerimizden başka class alamaz
+   //IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+   //new() :new'lenebilir olmalı
+   //şuanda sadece veritabanı nesnleriyele çalışabilen bir repostory oldu
+    
+    public interface IEntityRepository<T> where T : class, IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
